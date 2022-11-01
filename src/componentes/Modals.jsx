f793af4030ca38c93ee27a2338ff22eff99f2a71
JeimiBV <115201224+JeimiBV@users.noreply.github.com>
2022-11-01 13:11:09 -0400

    login con modal modificado

--- src/componentes/Modals.jsx
+++ src/componentes/Modals.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../estilos/Modals.css"
 
 
-function Modals({ children, estado, estadoPantalla,texto }) {
+function Modals({  estado, estadoPantalla,texto, titulo, mostrarSi, mostrarNo,buttons }) {
     return (
         <>
             {estado &&
@@ -11,13 +11,22 @@ function Modals({ children, estado, estadoPantalla,texto }) {
                 <div className="card-body position-relative text-center">
 
                 <i class="fa-solid fa-circle-exclamation fa-5x pb-3  "></i>
+                    <h2>
+                    {titulo}
+                    </h2>
                     <h4>
                         {texto}
                     </h4>
 
                 </div>
+                <div className={buttons?"botones":"transparent"}>
+                    <button type="submit" className="left" onClick={mostrarSi}>Si</button>
+                    <button className="right" onClick={mostrarNo}>No</button>
+                </div>
               </div>
                 </div>
+                //////////////////////////////////////
+               
             }
         </>
     )
