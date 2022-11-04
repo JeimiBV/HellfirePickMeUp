43aee1389ebfed043b08ad9d42cdffabe0bce48f
AdriNAT <115385238+AdriNAT@users.noreply.github.com>
2022-11-04 10:01:56 -0400

    Añadiendo componentes

--- src/componentes/Landing.jsx
+++ src/componentes/Landing.jsx
@@ -11,10 +11,10 @@ const Landing = () => {
   return (
     <div className='todo'> 
         
-        
+        <Carrusel/>
         <Informacion/>
         <Slider/>
-      
+        <MensajesP/>
         <Footer/>
     </div> 
 
