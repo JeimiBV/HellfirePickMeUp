f7e0ac0dd2af54c09cb6bcbfe97f3375075106dd
Adaluz Alvarez Santos <115201336+Adaluz-Alvarez-Santos@users.noreply.github.com>
2022-11-04 13:52:27 -0400

    Revert "Merge branch 'historia4y8' of https://github.com/JeimiBV/HellfirePickMeUp into historia4y8"
    This reverts commit 2f4267d5e4464b8b90a2bcacc493dd758d419707, reversing
    changes made to 14a3225f812a1395d96c55432515f373573b013f.

--- src/componentes/CompLand/CarruselInicio.jsx
+++ src/componentes/CompLand/CarruselInicio.jsx
@@ -19,35 +19,11 @@ export default class AutoPlay extends Component {
     };
     return (
         
-        <div className="fs-4 ">
-         <div className="sliderA text-center start-50 translate-middle-x">  
+        
         <Slider {...settings}>
-             <div>
-              <p>¡Darle una segunda oportunidad a los alimentos es darle un respiro al planeta!</p>
-            </div>
-            <div>
-              <p>Tomar buenas decisiones con la comida son buenas inversiones.</p>
-            </div>
-          
-            <div>
-              <p>Cualquier cosa es buena si está hecha de chocolate.</p>
-            </div>
-            <div>
-              <p>Cuida los alimentos, cuida el planeta.</p>
-            </div>
-            <div>
-              <p>Se debe comer para vivir, no vivir para comer.</p>
-            </div>
-            <div>
-              <p>Todo lo que necesitas es amor. Pero un poco de chocolate de vez en cuando, no hace daño.</p>
-            </div>
           
       
         </Slider>
-
-        </div>
-
-     </div>
     
     );
   }
