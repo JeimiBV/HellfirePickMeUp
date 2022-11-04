191ca5205a739c33cab204b4bf9299f034277929
Adaluz Alvarez Santos <115201336+Adaluz-Alvarez-Santos@users.noreply.github.com>
2022-11-04 09:59:15 -0400

    Añadiendo componentes

---
+++ src/componentes/Landing.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Inicio from './CompLand/Inicio'
+import Informacion from './CompLand/Informacion'
+import Carrusel from './CompLand/CarruselInicio'
+import Slider from './CompLand/Slider'
+import MensajesP from './CompLand/Mensajes'
+import Footer from './CompLand/Footer'
+
+const Landing = () => {
+  return (
+    <div className='todo'> 
+        
+        
+        <Informacion/>
+        <Slider/>
+      
+        <Footer/>
+    </div> 
+
+  )
+}
+export default Landing
