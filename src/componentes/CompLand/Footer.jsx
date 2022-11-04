f18237f21c82d668e3477af62a9192ed8a68b1a6
Adaluz Alvarez Santos <115201336+Adaluz-Alvarez-Santos@users.noreply.github.com>
2022-11-04 09:18:20 -0400

    Footer con datos

--- src/componentes/CompLand/Footer.jsx
+++ src/componentes/CompLand/Footer.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-top bg-sucess-2 border-dark border-1 '>
+        <div className='contenedor-footer row'>
+            <div className='col-sm'>
+                <h6 className='text-center'>
+                    Pick me up              
+                </h6>
+                <h6 className='text-center'>
+                    Salva la comida
+                </h6>
+   
+            </div>
+            
+        </div>
+    
+        
+    </footer>
   )
 }
 
