2ad3813e8b94493162558c6aa3b0781aeaa809a9
Adaluz Alvarez Santos <115201336+Adaluz-Alvarez-Santos@users.noreply.github.com>
2022-11-04 09:03:44 -0400

    Slider con imagenes

--- src/componentes/CompLand/Slider.jsx
+++ src/componentes/CompLand/Slider.jsx
@@ -15,6 +15,44 @@ export default class SwipeToSlide extends Component {
     };
 
     
-    
+    return (
+        <div>
+       
+          <div className="containerB top-110 start-50 translate-middle-x">
+            
+            <Slider {...settings}>
+              <div className = "container-slides-item">
+                <img src="../../../src/imagenes/imagen1.jpg" alt="..." width={270} height={200}/>
+              </div>
+              <div className = "container-slides-item">
+                <img src="../../../src/imagenes/imagen2.jpg" alt="..." width={270} height={200}/>
+              </div>
+              <div className = "container-slides-item">
+                <img src="../../../src/imagenes/imagen3.jpg" alt="..." width={270} height={200}/>
+              </div>
+              <div className = "container-slides-item">
+                <img src="../../../src/imagenes/imagen4.jpg" alt="..." width={270} height={200}/>
+              </div>
+              <div className = "container-slides-item">
+              <img src="../../../src/imagenes/imagen5.jpg" alt="..." width={270} height={200}/>
+              </div>
+              <div className = "container-slides-item">
+                <img src="../../../src/imagenes/imagen6.jpg" alt="..." width={270} height={200}/>
+              </div>
+              <div className = "container-slides-item">
+                <img src="../../../src/imagenes/imagen7.jpg" alt="..." width={270} height={200}/>
+              </div>
+              <div className = "container-slides-item">
+                <img src="../../../src/imagenes/imagen8.jpg" alt="..." width={270} height={200}/>
+              </div>
+              <div className = "container-slides-item">
+                <img src="../../../src/imagenes/imagen10.jpg" alt="..." width={270} height={200}/>
+              </div>
+            </Slider>
+  
+          </div>
+  
+        </div>
+      );
   }
 }
