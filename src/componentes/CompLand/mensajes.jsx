ace57f785a23a79c465813cd1fa834ae67154aeb
AdriNAT <115385238+AdriNAT@users.noreply.github.com>
2022-11-04 09:25:14 -0400

    mensajes de concientizacion

---
+++ src/componentes/CompLand/mensajes.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+export const MensajesP = () => {
+  return (
+
+      <div className=' p-5 top-150 start-20 '>
+        <div class="text-center body-1 ">
+          <div class="row body">
+            <div class="col-sm-8 p-5" top="50" size={20}  >
+                <p>   
+                  Nuestro sustento está íntimamente ligado a la comida que comemos 
+                  y al agua que bebemos. 
+                  Por eso tenemos que promover la responsabilidad y la conservación
+                  cuando se trata de nuestros recursos naturales.
+               </p>
+                  <p> -Mark Udal.</p>
+
+                  <p>  Cuando te levantes por la mañana, da gracias por la luz, por tu vida, por tu fuerza. 
+                  Pero especialmente, da las gracias por tu comida, ya que 
+                  es la que nos regala la alegría de vivir.</p>
+                    <p> - Tecumseh.</p>
+
+                 </div> 
+
+                 </div>
+    </div>
+    </div>
+
+
+  )
+}
+export default MensajesP
