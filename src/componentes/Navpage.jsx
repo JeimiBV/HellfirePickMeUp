110797dd91bedd1ddcf2ef7fabfcf76ae56ae647
Adaluz Alvarez Santos <115201336+Adaluz-Alvarez-Santos@users.noreply.github.com>
2022-11-04 14:02:20 -0400

    Arreglo

--- src/componentes/Navpage.jsx
+++ src/componentes/Navpage.jsx
@@ -5,16 +5,14 @@ import Informacion from "./Informacion";
 import Productoslista from "./ListaProductos";
 
 
-
 const NavPage = () =>{
     return(
 
              <Routes>
-                <Route path="/listaProductos" element={<Productoslista/>}/>
-                <Route path="/formulario" element={<Formulario />}/>
-                <Route path="/Informacion/:id" element={<Informacion/>}/>
-               
-            </Routes>
+                    <Route path="/listaProductos" element={<Productoslista/>}/>
+                    <Route path="/formulario" element={<Formulario />}/>
+                    <Route path="/Informacion/:id" element={<Informacion/>}/>
+                </Routes>
     )
 }
 
