5dd1cb678d8d470ff812ccb4f1753a9691779a14
Adaluz Alvarez Santos <115201336+Adaluz-Alvarez-Santos@users.noreply.github.com>
2022-11-04 08:53:06 -0400

    Creacion del Slider

---
+++ src/componentes/CompLand/Slider.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+
+
+export default class SwipeToSlide extends Component {
+  render() {
+    const settings = {
+      className: "center",
+      infinite: true,
+      centerPadding: "50px",
+      slidesToShow: 3,
+      swipeToSlide: true,
+      
+      
+    };
+
+    
+    
+  }
+}
