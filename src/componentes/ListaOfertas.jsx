9d9debb405662b8e3196dda6e137bd52e14f12d5
Adaluz Alvarez Santos <115201336+Adaluz-Alvarez-Santos@users.noreply.github.com>
2022-11-05 15:10:10 -0400

    llamada al back-end

--- src/componentes/ListaOfertas.jsx
+++ src/componentes/ListaOfertas.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import React, {useEffect, useState} from "react";
+import {todosProductos} from './funciones'
 
 const ListaOfertas = () => {
+    const [producto, setProducto]=useState(null)
+    useEffect(() => {
+        todosProductos(setProducto)
+    },[] )
+
   return (
     <div>ListaOfertas</div>
   )
