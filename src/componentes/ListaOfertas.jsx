cc9069fe72e4a7ec31994869e413aed24dcb56f4
AdriNAT <115385238+AdriNAT@users.noreply.github.com>
2022-11-05 15:32:46 -0400

    datos card

--- src/componentes/ListaOfertas.jsx
+++ src/componentes/ListaOfertas.jsx
@@ -20,15 +20,20 @@ const ListaOfertas = () => {
         <div className="row row-col" >  
                 <div class="card border-success mb-2 bg-sucess">
                     <img src={oferta.Imagen} class="card-img-top mt-2 mr-2  " alt="..." width='50' height='150'/>         
-                     </div>
-                         </div>
+                    <div class="card-body text-capitalize">
+                            <h5 class="card-title text-center " >{oferta.Nombre}</h5>                                         
+                              <p > 
+                                <span > Precio: {oferta.Precio} </span> 
+                                <span className="d-block"> Hora Limite:{oferta.Hora} </span>  
+                                <span className="d-block">Fecha Limite: {oferta.Fecha} </span> 
+                            </p>   
+                        </div>                     
+                </div>
+            </div>
+        </div>
+    )
             
-
-
-
-
-
-  return (
+   return (
     
         <div className="container">                                                                                                                                             <h1 className=" text-center " > Lista de Ofertas </h1>      
             <div className="ofertas"> {lista}</div>
