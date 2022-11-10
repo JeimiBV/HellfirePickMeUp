5851961bcbf84a515011f1e58f4b9d1442b4d88c
majoPS <majito9115@gmail.com>
2022-11-10 11:27:57 -0400

    HU5 aumentado

--- src/componentes/Oferta.jsx
+++ src/componentes/Oferta.jsx
@@ -4,16 +4,19 @@ import { useParams} from "react-router-dom";
 import {filtrarOfertas} from './funciones'
 import { Navigate } from "react-router-dom";
 import { useAuth } from '../context/authContext';
+import {todosCategorias} from "./funciones";
 function Mostraroferta({usuario}){
     const { user } = useAuth();
      
     if(usuario!=user.uid){
         return <Navigate to ="/"/>
     }
+    const [categorias , setCategorias] = useState(null);
     const [ofertas, setOfertas]=useState(null)
     const params = useParams()
     useEffect(() => {  
         filtrarOfertas(params.id,setOfertas)
+        todosCategorias(setCategorias)
     },[] )
     console.log(ofertas)
 
@@ -28,10 +31,10 @@ function Mostraroferta({usuario}){
         <div class="list-card" key={oferta.id}>
             
                 <section>
-                    <h1>{oferta.Nombre}</h1>
-                    <span class="price">Precio: {oferta.Precio}</span><br/>
+                    <h1 className="nombreO">{oferta.Nombre}</h1>
+                    <span class="price">Precio: {oferta.Precio}   Bs</span><br/>
                     
-                    <span>Hora limite: {oferta.Hora} </span>
+                    <span class="price">Hora límite: {oferta.Hora} </span>
                    
                 </section>
                 <section className="seccion1">
@@ -42,6 +45,13 @@ function Mostraroferta({usuario}){
                 </section>
             </div>
       )
+)
+const pr1 = categorias || []
+const ListaCategorias = pr1.map(categoria => (
+
+          <a href={`/Oferta/${categoria.id}`}>{categoria.Name}</a>
+    
+        )
 )
 return(
     <div className="container89">
@@ -54,13 +64,8 @@ return(
             <div class="list-section">
                 <div class="left-section">
                     <section class="column-1">
-                        <h1>Categorias</h1>
-                            <a href="/Oferta/0">Busca tu almuerzo o cena</a>
-                            <a href="/Oferta/1">Estas con antojos</a>
-                            <a href="/Oferta/2">Escoge tu cafe y postre</a>
-                            <a href="/Oferta/3">Escoge tu bebida</a>
-                            <a href="/Oferta/4">No olvides lo escencial</a>
-                            <a href="/Oferta/5">Snack</a>
+                        <h1>Categorías</h1>
+                          {ListaCategorias} 
                     </section>
                 </div>
                 <div class="right-section">
