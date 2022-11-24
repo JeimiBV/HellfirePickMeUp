38e5035dfdd34aa325943839aca8c309d2e104df
vania <115201154+Liz1610@users.noreply.github.com>
2022-11-24 13:02:19 -0400

    boton de cerrar

--- src/componentes/Consumidor/ListaPedidos.jsx
+++ src/componentes/Consumidor/ListaPedidos.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState, setState } from "react";
-import { todosProductos } from '../funciones'
+import { todosPedidos } from "../funciones";
 import '../../estilos/estilosPedidos.css';
 import { useAuth } from "../../context/authContext";
 import { getDownloadURL } from "firebase/storage";
-
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 const ListaPedidos = ({ usuario }) => {
 
     const { user } = useAuth();
@@ -12,28 +13,52 @@ const ListaPedidos = ({ usuario }) => {
         return <Navigate to="/" />
     }
 
-    const [productos, setProductos] = useState(null)
-    const [pedido, setPedidos] = useState(null)
-    //const params = useParams()
+    const [pedidos, setPedidos] = useState(null)
+    const [post, setPost] = useState(null);
+    const url = "https://us-central1-base-de-datos-h.cloudfunctions.net/app/api/pedido";
+    //const [productos, setProductos] = useState(null)
+    //const [pedido, setPedidos] = useState(null)
+    const params = useParams()
     useEffect(() => {
-        todosProductos(setProductos)
-    }, [])
+        todosPedidos(setPedidos)
+        if (post !== null) {
+            axios.get(`${url}/1`).then((response) => {
+                setPost(response.data);
+            });
+        }
+    }, [post])
+    function updatePed() {
+        axios
+            .put(`${url}/${params.id}`, {
+                Imagen: pedidos.Imagen,
+                PrecioUnitario: pedidos.PrecioUnitario,
+                PrecioTotal: pedidos.PrecioTotal,
+                Hora: pedidos.Hora,
+                FechaLimite: pedidos.FechaLimite,
+                Cantidad: pedidos.Cantidad,
+                Nota: pedidos.Nota,
+                FlagC: false,
+                FalgN: pedidos.FlagN
+            })
+            .then((response) => {
+                setPost(response.data);
+            });
+    }
 
     const fechaActual = new Date()
-    const pr = productos || []
+    const pr = pedidos || []
 
-    const listaP = pr.filter(producto => {
-        const fechaPr = new Date(producto.Fecha + 'T' + producto.Hora)
-        return fechaPr > fechaActual && producto.Precio !== '';
+    const listaP = pr.filter(pedido => {
+        const fechaPr = new Date(pedido.FechaLimite + 'T' + pedido.Hora)
+        return fechaPr > fechaActual && pedido.PrecioTotal !== '' && pedido.FlagC == true;
     }
     )
 
-
-    const deleteTask = (id) => {
-        const filteredTasks = pr.filter(producto => producto.id !== id)
-        setProductos(filteredTasks)
+    /*const deleteTask = (id) => {
+        const filteredTasks = pr.filter(pedido => pedido.id !== id)
+        setPedidos(filteredTasks)
         
-    }
+    }*/
 
     return (
 
@@ -43,24 +68,24 @@ const ListaPedidos = ({ usuario }) => {
                 <div className="PedidosP">
 
                     {listaP != null ? (
-                        listaP.map(pedidos => (
-                            <div className="row row-col"  key={pedidos.id}>
+                        listaP.map(pedidosC => (
+                            <div className="row row-col"  key={pedidosC.id}>
                                 <div class="cards" >
                                     <div class="link">
-                                        <h5 class="card-tituloNombre text-center ">{pedidos.Nombre}</h5>
+                                        <h5 class="card-tituloNombre text-center ">{pedidosC.Nombre}</h5>
                                     </div>
                                     <div class="face front">
-                                        <img src={pedidos.Imagen} alt=" " />
+                                        <img src={pedidosC.Imagen} alt=" " />
                                     </div>
                                     <div className=" face back" >
                                         <p className="" >
-                                            <span > Precio: {pedidos.Precio} bs.</span>
-                                            <span className="d-block"> Hora Límite:{pedidos.Hora} </span>
-                                            <span className="d-block">Fecha Límite: {pedidos.Fecha} </span>
-                                            <span className="d-block">Cantidad: {pedidos.Precio} </span>
+                                            <span > Precio: {pedidosC.PrecioTotal} bs.</span>
+                                            <span className="d-block"> Hora Límite:{pedidosC.Hora} </span>
+                                            <span className="d-block">Fecha Límite: {pedidosC.FechaLimite} </span>
+                                            <span className="d-block">Cantidad: {pedidosC.Cantidad} </span>
                                         </p>
                                     </div>
-                                    <button className="botonEliminar" color="danger" onClick={() =>deleteTask(pedidos.id)} >
+                                    <button className="botonEliminar" color="danger" onClick={updatePed} >
                                     <i class="fa-regular fa-trash-can"></i></button>
                                 </div>
                             
