aaf4b77ff2e668114215c96709be397c92cbfdac
AdriNAT <115385238+AdriNAT@users.noreply.github.com>
2022-11-26 00:47:35 -0400

    VistaConsumidor.jsx

--- src/componentes/Consumidor/VistaConsumidor.jsx
+++ src/componentes/Consumidor/VistaConsumidor.jsx
@@ -1,5 +1,101 @@
-const BuscarProducto = ({ usuario }) => {
+import React, { useEffect, useState } from "react";
+import '../../estilos/BusquedaOfertas.css'
+import { useAuth } from '../../context/authContext';
+import { Navigate } from "react-router-dom";
+import {todosProductos} from '../funciones'
 
-}
 
-export default BuscarProducto
+const VistaConsumidor = ({usuario}) => {
+
+    const { user } = useAuth();
+    
+            if(usuario!=user.uid){
+              return  <Navigate to = "/"/>
+            }
+
+            const [productos, setProductos] = useState([])
+            const [tablaProductos, setTablaProductos]= useState([]);
+            const [busqueda, setBusqueda]= useState("");
+        
+            useEffect(() => {
+                todosProductos(setProductos)
+                todosProductos(setTablaProductos)
+            }, [])
+        
+            const handleChange=e=>{
+        
+                if (/\s/.test(e.target.value)) {
+                    e.target.value = "";
+                    setBusqueda(e.target.value);
+                    filtrar(e.target.value); 
+                }else{
+                    filtrar(e.target.value); 
+                }                  
+            }
+        
+            const fechaActual = new Date()  
+        
+            const ofertados = productos.filter(producto => 
+                {   
+                    const fechaPr = new Date(producto.Fecha + 'T' + producto.Hora) 
+                    return  fechaPr > fechaActual && producto.Precio !== '';
+                }
+            ) 
+          
+            const filtrar=(terminoBusqueda)=>{
+                var resultadosBusqueda= tablaProductos.filter((elemento)=>{
+                    const fechaPr = new Date(elemento.Fecha + 'T' + elemento.Hora)
+                  if(elemento.Nombre.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())){
+                    return fechaPr > fechaActual && elemento.Precio !== '';
+                  }
+                });
+                setProductos(resultadosBusqueda);
+              }  
+            
+            return (
+        
+                <>
+                    
+        
+                        <div className="ofertasDelosProductos"> 
+                        
+                            { 
+                                ofertados.map(oferta => (
+                                    <div  className="row row-col" key={oferta.id}>
+                                        <div class="card card-ofP  mb-2 bg-sucessPedido">
+                                        <span class="Banner">
+                                        <h1 className="Descri">Descripción :</h1>
+
+                                                 <p>{oferta.Descripcion} </p>
+                                                                    </span>
+                                                                    
+                                            
+                                            <img src={oferta.Imagen} class="card-img-top imagenOf mt-2 mr-2" alt="..." />
+                                            
+                                            
+                                            <div className="DisenioCard ">
+                                            <div class="card-body card-letra text-capitalize">
+                                                <h5 class="card-titleProducto text-center " >{oferta.Nombre}</h5>
+                                                <p className="">
+                                            
+                                                    <span > Precio: {oferta.Precio} bs.</span>
+                                                    <span className="d-block"> Hora Límite:{oferta.Hora} </span>
+                                                    <span className="d-block">Fecha Límite: {oferta.Fecha} </span>
+                                                    
+                                                </p>
+                                              </div>
+                                            </div>
+                                        </div>
+        
+                                    </div>
+                                )
+                                )
+                           }
+                        </div>
+                    </div>
+        
+                </>
+            )
+        }
+
+export default VistaConsumidor
