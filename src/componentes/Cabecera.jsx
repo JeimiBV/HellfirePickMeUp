426864ab25a0fd632fce9f788e272fb044d98c84
Adaluz Alvarez Santos <115201336+Adaluz-Alvarez-Santos@users.noreply.github.com>
2022-12-07 14:20:55 -0400

    logo-nav

--- src/componentes/Cabecera.jsx
+++ src/componentes/Cabecera.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "../../public/log.png";
+import Image from "../../public/logo-9.png";
 import "../estilos/header.css";
 import { useAuth } from "../context/authContext";
 import { Link, useNavigate } from "react-router-dom";
@@ -19,13 +19,12 @@ function Header() {
   return (
     <div className="contenedor-cabecera">
        <nav className="navbar fixed-top navbar-expand-sm bg-sucessN border-bottom border-dark border-2 ">
-        <div className="d-flex  w-50  justify-content">
-       
-            <div className="logoYNombre mx-5">
-            <Link className="flex-sm-fill  nav-link active" to="/">
-              <img className="logo" src={Image}  />
-            </Link>    
-            </div >
+          <div className="d-flex  w-50  justify-content">
+              <div className="logoYNombre mx-5">
+              <Link className="flex-sm-fill  nav-link active" to="/">
+                <img className="logo mx-5" src={Image}  />
+              </Link>    
+          </div >
          
                  
         </div>
