64ffe71e488204022e36179a91916cce8c06fb42
AdriNAT <115385238+AdriNAT@users.noreply.github.com>
2022-12-08 22:23:50 -0400

    ArregloMensajesLan

--- src/componentes/CompLand/mensajes.jsx
+++ src/componentes/CompLand/mensajes.jsx
@@ -33,26 +33,26 @@ export const MensajesP = () => {
                  <div class="col-sm p-5">
                    <img className=""  src="https://previews.123rf.com/images/vectorv/vectorv1907/vectorv190706587/126254750-papelera-de-reciclaje-naranja-con-icono-de-s%C3%ADmbolo-de-reciclaje-aislado-sobre-fondo-blanco-icono-de-.jpg" width="90" height="90"/>
                    <div class="MensajeLanding">
-                   <h3>
+                   <h10>
                      Usando la aplicación ayudas a reducir 
                      el desperdicio de comida
-                   </h3>
+                   </h10>
                  </div>
                  </div>
                  <div class="col-sm p-5">
                      <img className=""  src="https://cdn.icon-icons.com/icons2/217/PNG/512/user-orange_25327.png" width="90" height="90"/>
                     <div class="MensajeLanding">
-                    <h3>
+                    <h10>
                     ¡Ya son 1000 usuarios que tienen la App!
-                    </h3>   
+                    </h10>   
                     </div>
                     </div>
                     <div class="col-sm p-5">
                      <img className=""  src="https://img.freepik.com/vector-premium/rebanada-redonda-naranja-vector-ilustracion-realista-aislada-sobre-fondo-blanco-predisenadas-icono-eps-naranja_335478-830.jpg?w=740" width="90" height="90"/>
                      <div class="MensajeLanding">
-                     <h3>
+                     <h10>
                     ¡Tenemos variedad de ofertas para ti!
-                    </h3>   
+                    </h10>   
                     </div>
                  </div> 
 
