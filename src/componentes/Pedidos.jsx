9c5d21e3a4e9983d14dec31985762b0f8aed69ba
Adaluz Alvarez Santos <115201336+Adaluz-Alvarez-Santos@users.noreply.github.com>
2022-12-11 15:25:26 -0400

    Arreglos buscador pedidos

--- src/componentes/Pedidos.jsx
+++ src/componentes/Pedidos.jsx
@@ -108,51 +108,58 @@ function MostrarPedidosN({ usuario }) {
     
     )
     
-    if(validacion > 0){
-        
-        return(
-           
-           <div className="containerG mt-3">
-           
-            <h1 className="titleL"> Lista de pedidos </h1> 
-                       <div class="container-fluid">
-       
-                        <div className="buscador">
-                           <form class="d-flex justify-content-center" value={busqueda}  role="search" onChange={handleChange}>
-                            <i class="bi bi-search px-3"></i>
-                               <input class="form-control inputBusc me-2 px-4" type="search" placeholder=" Ingrese el nombre del pedido..." aria-label="Search"  />              
-                          </form>
-                       </div>
-       
-                            <div id="fda_app" class="row">
-                   
-                                    <section id="fda_product_tile" class="col-12">
-                               
-                                       <div class="row fda_food_row">
-                                           
-                                           {ListaPedidos}
-                                       
-                                        </div>
-       
-                                    </section>
-                   
-                            </div>
-                    </div>
-             </div>
-       
-       
-         )
-        
-    
-     }else{
-        return (<div className="containerG mt-3">
-           
-        <h1 className="titleL"> Lista de pedidos </h1> 
-           <div>No hay pedidos </div>
-        </div>)
-            
-     }
-    
+    return (
+        <div>
+      <div className="containerG mt-3">
+        <h1 className="titleL"> Lista de pedidos </h1>
+        <div className="buscador">
+          <form class="d-flex justify-content-center" value={busqueda} role="search" onChange={handleChange}>
+            <i class="bi bi-search px-3"></i>
+            <input class="form-control inputBusc me-2 px-4" type="search" placeholder=" Ingrese el nombre del pedido..." aria-label="Search"/>
+          </form>
+        </div>
+        <div class="container-fluid">
+          <div id="fda_app" class="row">
+            <section id="fda_product_tile" class="col-12">
+              <div class="row fda_food_row">
+              {pedidos1 != null ? (
+                        pedidos1.map(pedido => (
+              <div class="col-9" key={pedido.id}>
+                <div class="food_tile active">
+                    <img src={pedido.Imagen} alt="" class="fda_product_img" />
 
-}
+                    <span class="food_name">{pedido.Nombre}</span>
+
+                    <span class="food_detail">{pedido.Nota}</span>
+                    <div class="botonN">
+                    <ul id="food_meta">
+                        <li>
+                        Cantidad:
+                        <span>{pedido.Cantidad}</span>
+                        </li>
+                        <li>
+                        Precio total:
+                        <span>Bs {pedido.PrecioUnitario}</span>
+                        </li>
+                    </ul>
+
+                    <button
+                        type="button" class="btn btn-sm btn-default" onClick={() => { updatePedido(pedido); }}>
+                        Entregado
+                    </button>
+                    </div>
+                </div>
+                </div>
+               ) )) : ('No hay pedidos ')}
+               
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+      </div>
+
+    );
+    
+    }
 export default MostrarPedidosN;
