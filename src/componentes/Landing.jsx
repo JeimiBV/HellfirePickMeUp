76fa6ede4d959ca2af2ded246bb1e8f5d46a3892
Adaluz Alvarez Santos <115201336+Adaluz-Alvarez-Santos@users.noreply.github.com>
2022-12-11 20:25:11 -0400

    color fondo landing

--- src/componentes/Landing.jsx
+++ src/componentes/Landing.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 import Informacion from './CompLand/Informacion'
 import Carrusel from './CompLand/CarruselInicio'
-import Slider from './CompLand/Slider'
 import MensajesP from './CompLand/mensajes'
 import Footer from './CompLand/Footer'
 
 const Landing = () => {
   return (
-    <div > 
+    <div className='bg-white'> 
         <Carrusel/>
         <Informacion/>
         <MensajesP/>
